89018e3d4404ca8d176d94d8c72f0902e4b5ebaf
asifhajiyev <asifhaciyev1498@gmail.com>
2020-04-30 03:55:14 +0400

    BE-50: creating configuration for FeignClient in different class

--- src/main/java/az/gdg/msalarm/MsAlarmApplication.java
+++ src/main/java/az/gdg/msalarm/MsAlarmApplication.java
@@ -2,10 +2,8 @@ package az.gdg.msalarm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.retry.annotation.EnableRetry;
 
-@EnableFeignClients
 @EnableRetry
 @SpringBootApplication
 public class MsAlarmApplication {
---
+++ src/main/java/az/gdg/msalarm/config/FeignConfig.java
@@ -0,0 +1,17 @@
+package az.gdg.msalarm.config;
+
+import az.gdg.msalarm.client.MsArticleClient;
+import az.gdg.msalarm.client.MsAuthClient;
+import az.gdg.msalarm.client.MsComplaintClient;
+import az.gdg.msalarm.client.MsMailClient;
+import az.gdg.msalarm.client.MsSubscriberClient;
+import az.gdg.msalarm.client.MsTeamClient;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(clients = {MsArticleClient.class, MsAuthClient.class, MsComplaintClient.class,
+        MsMailClient.class, MsSubscriberClient.class, MsTeamClient.class})
+public class FeignConfig {
+}
