7eba4a88bec23b099b312f61d9ba74ff895516f2
asifhajiyev <asifhaciyev1498@gmail.com>
2020-04-30 22:22:43 +0400

    BE-50: modifying client urls

--- src/main/java/az/gdg/msalarm/client/MsAlarmClient.java
+++ src/main/java/az/gdg/msalarm/client/MsAlarmClient.java
@@ -3,7 +3,7 @@ package az.gdg.msalarm.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(value = "ms-alarm-client", url = "https://ms-alarm-gdg.herokuapp.com/alarm")
+@FeignClient(value = "ms-alarm-client", url = "https://gdg-ms-alarm.herokuapp.com/alarm")
 public interface MsAlarmClient {
     @GetMapping
     void invokeMsAlarm();
--- src/main/java/az/gdg/msalarm/client/MsTeamClient.java
+++ src/main/java/az/gdg/msalarm/client/MsTeamClient.java
@@ -3,7 +3,7 @@ package az.gdg.msalarm.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(value = "ms-team-client", url = "https://gdg-ms-team.herokuapp.com/api/health")
+@FeignClient(value = "ms-team-client", url = "https://ms-team-gdg.herokuapp.com/api/health")
 public interface MsTeamClient {
     @GetMapping
     void invokeMsTeam();
