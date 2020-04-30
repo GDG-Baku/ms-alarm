543dd7f9abd1cbc132a94f18772697043a3dd9d1
asifhajiyev <asifhaciyev1498@gmail.com>
2020-04-30 04:10:27 +0400

    BE-50: creating configuration for Retry in different class and fixing client url

--- src/main/java/az/gdg/msalarm/MsAlarmApplication.java
+++ src/main/java/az/gdg/msalarm/MsAlarmApplication.java
@@ -2,9 +2,7 @@ package az.gdg.msalarm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.retry.annotation.EnableRetry;
 
-@EnableRetry
 @SpringBootApplication
 public class MsAlarmApplication {
 
---
+++ src/main/java/az/gdg/msalarm/config/RetryConfig.java
@@ -0,0 +1,9 @@
+package az.gdg.msalarm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@Configuration
+@EnableRetry
+public class RetryConfig {
+}
