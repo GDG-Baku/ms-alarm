ca4adfc5fe07cea6f92a9a25cbb85994681cefb5
asifhajiyev <asifhaciyev1498@gmail.com>
2020-05-14 18:10:58 +0400

    BE-50: deleting ArticleClient class

--- src/main/java/az/gdg/msalarm/config/FeignConfig.java
+++ src/main/java/az/gdg/msalarm/config/FeignConfig.java
@@ -1,7 +1,6 @@
 package az.gdg.msalarm.config;
 
 import az.gdg.msalarm.client.AlarmClient;
-import az.gdg.msalarm.client.ArticleClient;
 import az.gdg.msalarm.client.AuthClient;
 import az.gdg.msalarm.client.ComplaintClient;
 import az.gdg.msalarm.client.MailClient;
@@ -12,7 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableFeignClients(clients = {ArticleClient.class, AuthClient.class, ComplaintClient.class,
+@EnableFeignClients(clients = {AuthClient.class, ComplaintClient.class,
         MailClient.class, SubscriberClient.class, TeamClient.class, AlarmClient.class})
 public class FeignConfig {
 }
